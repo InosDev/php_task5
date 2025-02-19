de479093e1ee4ceaa92069e066fed184cea6ce6f
InosDev <inosbog@gmail.com>
2025-02-19 14:27:13 +0300

    comm 2

--- index.php
+++ index.php
@@ -5,36 +5,55 @@ declare(strict_types = 1);
 function calcWorkingDays(int $numYear, int $numMonth): void
 {
 
-    //$sumDaysMonth = date('t');
     $sumDaysMonth = cal_days_in_month(CAL_GREGORIAN, $numMonth, $numYear);
 
-    $nDays = 1;
+    $monthName = date("F", mktime(0, 0, 0, $numMonth, 1, $numYear));
+
+    echo $monthName . PHP_EOL;
+
+    $mondayWork = false;
 
     for ($i = 1; $i <= $sumDaysMonth; $i++)
     {
         
-        if ($nDays != 6 && $nDays != 7)
+        $wDaysWeek = date("w", mktime(0, 0, 0, $numMonth, $i, $numYear));
+        $dmDaysWeek = date("d M", mktime(0, 0, 0, $numMonth, $i, $numYear));
+
+        $isWorkDay = isWorkDay($i);
+
+        if (($wDaysWeek == 6 && $wDaysWeek == 0) && $isWorkDay)
         {
-
-            echo $i . " +" . PHP_EOL;    
-
-        } else
-        {
-            echo $i  . PHP_EOL;     
-        }
-
-        if ($nDays == 7)
-        {
-            $nDays = 1;
-
+            $mondayWork = true;
         }else
         {
-            $nDays++;    
+            $mondayWork = false;   
+        }
+
+       if ($wDaysWeek == 1 && $mondayWork)
+        {
+            echo $dmDaysWeek . "+". PHP_EOL;
+           continue;
+        }
+
+
+        if ($isWorkDay && ($wDaysWeek <> 6 && $wDaysWeek <> 0))
+        {
+            echo $dmDaysWeek . "+". PHP_EOL;     
+        }else
+        {
+            echo $dmDaysWeek . PHP_EOL;    
         }
   
     }   
 
 }
+
+function isWorkDay(int $numDay): bool 
+{
+    return ($numDay % 4) < 2;
+};
+
+
 
 $result = calcWorkingDays(2025, 1);  
 
